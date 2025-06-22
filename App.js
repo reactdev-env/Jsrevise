86b275075496938f9a34881b238a0c25bfcb650a
pavan sai <pavansaipcr@gmail.com>
2025-06-23 02:01:40 +0530

    Functions examples

--- App.js
+++ App.js
@@ -3,3 +3,40 @@ function person(fname,lname,age,ht,wt){
 }
 
 person('pavan sai','pasupuleti','25','5.7','95')
+
+//Calculating a value
+
+function square(number){
+    return number * number;
+}
+
+let op=square(2);
+console.log(op);
+
+//pi
+
+function pi(){
+    return Math.PI;
+}
+
+console.log(pi())
+
+
+//Twotable
+
+function printTable(num) {
+  for (let i = 1; i <= 10; i++) {
+    document.writeln(`${num} x ${i} = ${num * i}<br>`);
+  }
+}
+
+printTable(2);
+
+
+//square of two
+
+function sqtwo(num){      // This is the another function
+    return num * num;
+}
+
+console.log(square(2));     // This is the one function 
