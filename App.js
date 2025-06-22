0a01e203535ac84e895096a833aebb2fd789e1eb
pavan sai <pavansaipcr@gmail.com>
2025-06-22 21:13:20 +0530

    Objects

--- App.js
+++ App.js
@@ -1,3 +1,11 @@
-const intrestRate= 0.1;
-intrestRate=1;
-document.writeln(intrestRate)
+const person = {
+    name:'pavan',
+    age:25,
+    ht:5.11,
+    wt:97,
+    City:'Hyderabd',
+    State:'Telangana'
+
+}
+
+console.log(person)
