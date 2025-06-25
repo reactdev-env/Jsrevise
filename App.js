bbd40ca6df823e8eb1359517e9137b2385f67c7d
pavan sai <pavansaipcr@gmail.com>
2025-06-25 13:20:25 +0530

    statementy,expression,anonymus and named

--- App.js
+++ App.js
@@ -1,42 +1,33 @@
-function person(fname,lname,age,ht,wt){
-    console.log(fname+ ' ' + lname + ' ' + age + ' ' +ht + ' ' + wt)
+a();
+b();
+
+
+//Function statement aka declaration
+function a() {
+  console.log("This is the function");
 }
 
-person('pavan sai','pasupuleti','25','5.7','95')
+//function expression
 
-//Calculating a value
-
-function square(number){
-    return number * number;
+var b = function (){
+  console.log("This is the b function");
 }
 
-let op=square(2);
-console.log(op);
+//Anonymus function
 
-//pi
+//function () {
+  console.log("This is the anonymus function")
+//}
 
-function pi(){
-    return Math.PI;
+//Named function
+
+var c= function xyz(){
+  console.log("Named function")
 }
 
-console.log(pi())
+c();
 
 
-//Twotable
-
-function printTable(num) {
-  for (let i = 1; i <= 10; i++) {
-    document.writeln(`${num} x ${i} = ${num * i}<br>`);
-  }
-}
-
-printTable(2);
 
 
-//square of two
 
-function sqtwo(num){      // This is the another function
-    return num * num;
-}
-
-console.log(square(2));     // This is the one function 
